09b388bed14f31c14add70bed148be1ef53333d7
Yasmin Nathaly <ynathaly58@gmail.com>
2025-10-06 08:38:55 -0300

    first commit

---
+++ crud.py
@@ -0,0 +1,59 @@
+from db import connect 
+def criar_aluno(nome, idade):
+    conexao, cursor = connect()
+    if conexao:
+        try:
+            cursor.execute(
+                "INSERT INTO alunos (nome, idade) VALUES (%s, %s)", 
+                (nome, idade)
+            )
+            conexao.commit()
+        except Exception as erro:
+            print(f'Erro ao inserir: {erro}')
+        finally:
+            cursor.close()
+            conexao.close()
+def listar_alunos():
+    conexao, cursor = connect()
+    if conexao:
+        try:
+            cursor.execute(
+                "SELECT * FROM alunos ORDER BY id" # ordena por id
+                )
+            return cursor.fetchall()  # fetchall traz todos os registros
+        except Exception as erro:
+            print(f'Erro ao listar: {erro}')
+            return [] # retorna lista vazia em caso de erro
+        finally: 
+            cursor.close()
+            conexao.close()
+
+def atualizar_alunos(id_aluno, nova_idade):
+    conexao, cursor = connect()
+    if conexao:
+        try:
+            cursor.execute( 
+                "UPDATE alunos SET idade = %s WHERE id = %s", 
+                (nova_idade, id_aluno)
+            )
+        except Exception as erro:
+            print(f'Erro ao atualizar um aluno: {erro}')
+        finally:
+                
+            cursor.close()
+            conexao.close()
+
+def deletar_aluno(id_aluno):
+    conexao, cursor = connect()
+    if conexao:
+        try:
+            cursor.execute(
+                "DELETE FROM alunos WHERE id = %s", 
+                (id_aluno,)
+            )
+            conexao.commit()
+        except Exception as erro:
+            print(f'Erro ao deletar um aluno: {erro}')
+        finally:
+            cursor.close()
+            conexao.close()
